a422e210b31cae57c555bb8307816870bf930c93
Unknown <thanh.hoquangcse96@gmail.com>
2018-10-18 17:39:21 +0700

    Update Views

--- mysite/getnewsurl/urls.py
+++ mysite/getnewsurl/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #ex: /getnewsurl/
     path('', views.index, name='index'),
+
+    # view news, ex: /getnewsurl/5
+    path('<int:news_id>/', views.viewNews, name='news-details'),
+
+    # view news stance, ex: getnewsurl/5/stance 
+    path('<int:news_id>/stance', views.viewNewsStance, name='news-stance'),
 ]
--- mysite/getnewsurl/views.py
+++ mysite/getnewsurl/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import News
 
 # Create your views here.
 def index(request):
-    return HttpResponse("")
+    lastest_news_list = News.objects.order_by('-news_time_publish')[:5]
+    template = loader.get_template('getnewsurl/index.html')
+    context = {
+        'lastest_news_list': lastest_news_list,
+    }
+    return HttpResponse(template.render(context, request))
     
 def getUrl(request):
-    pass
+    response = "Trang lấy tin tức theo URL"
+    return HttpResponse(response)
 
 def viewNews(request, news_id):
-    response = "Bạn đang xem tin tức số %s trong dữ liệu."
-    return HttpResponse(response % news_id)
+    try:
+        news_details = News.objects.get(pk=news_id)
+    except News.DoesNotExist:
+        raise Http404("Chưa tồn tại tin tức cần duyệt")
+    return render(request,'getnewsurl/news_content.html', {'content': news_details})
+    # response = "Bạn đang xem tin tức số %s trong dữ liệu."
+    # return HttpResponse(response % news_id)
 
-def viewNewsStance(request, news_stance_id):
+def viewNewsStance(request, news_id):
     response = "Bạn đang xem lập trường của tin tức số %s trong dữ liệu."
-    return HttpResponse(response % news_stance_id)
+    return HttpResponse(response % news_id)
